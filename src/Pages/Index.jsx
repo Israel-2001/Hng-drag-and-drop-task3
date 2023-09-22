3908ef6e31dc4b19411d472d02f49002c6131be2
SOLOMON ISRAEL <75482038+Israel-2001@users.noreply.github.com>
2023-09-22 19:02:43 +0100

    Update Index.jsx

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -89,25 +89,6 @@ export default function SignIn() {
               />
             )}
             </div>
-            <div 
-              className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
-              <p className="mb-6 text-white">Don't have a account? 
-                <Link className="text-red-600 hover:text-red-700 
-                  transition duration-200  ease-out ml-1" 
-                  to="/sign-up"
-                >
-                  Register
-                </Link>
-              </p>
-              <p>
-                <Link className="text-blue-600 hover:text-blue-700 
-                  transition duration-200  ease-out" 
-                  to="/forgot-password"
-                >
-                  Forgot-password?
-                </Link>
-              </p>
-            </div>
           <button className="w-full bg-blue-600 
             text-white px-7 py-3 text-sm 
             font-medium uppercase rounded 
